8c5c7e8c6eeae20d573124ddcd912b435e577903
DarkCode <josemanuelramirezrengifo9@gmail.com>
2026-04-21 12:16:24 -0400

    Implement DataBase class for inventory management
    Add DataBase class for SQLite database management with methods to create a table and insert data.

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
 import sqlite3 as sql
+
+class DataBase:
+    def __init__(self):
+        self.conn = sql.connect("inventory.db")
+        self.conn.commit()
+        self.conn.close()
+
+        self.create_table()
+
+    def create_table(self):
+        try:
+            self.conn = sql.connect("inventory.db")
+            self.cursor = self.conn.cursor()
+            self.cursor.execute(
+              """CREATE TABLE IF NOT EXISTS inventory(
+                    name TEXT,
+                    last_name TEXT,
+                    age INTEGER
+              )"""
+            )
+        finally:
+            self.conn.commit()
+            self.conn.close()
+
+    def insert_data(self):
+        try:
+            self.conn = sql.connect("inventory.db")
+            self.cursor = self.conn.cursor()
+            self.instruction = f"INSERT INTO inventory VALUES('{name}')"ñ
+          
+            self.cursor.execute()
+
+if __name__ == '__main__':
+    pass

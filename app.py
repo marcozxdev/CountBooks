fd0b5e0a20b49ecf3830902ba30ce9b18d83480e
DarkCode <josemanuelramirezrengifo9@gmail.com>
2026-04-25 17:42:18 -0400

    Add DataBase class for SQLite inventory management
    Implement a basic SQLite database class with table creation and data insertion methods.

---
+++ app.py
@@ -0,0 +1,37 @@
+import sqlite3 as sql
+
+class DataBase:
+    def __init__(self):
+        self.conn = sql.connect("inventory.db")
+        self.conn.commit()
+        self.conn.close()
+
+        self.create_table()
+
+    def create_table(self):
+        try:
+            self.conn = sql.connect("inventory.db")
+            self.cursor = self.conn.cursor()
+            self.cursor.execute(
+              """CREATE TABLE IF NOT EXISTS inventory(
+                    name TEXT,
+                    last_name TEXT,
+                    age INTEGER
+              )"""
+            )
+        finally:
+            self.conn.commit()
+            self.conn.close()
+
+    def insert_data(self):
+        try:
+            self.conn = sql.connect("inventory.db")
+            self.cursor = self.conn.cursor()
+            self.instruction = f"INSERT INTO inventory VALUES('{name}')"
+          
+            self.cursor.execute(self.instruction) # Critical vulnerability: SQL Inyection
+        finally:
+            self.conn.commit()
+            self.conn.close()
+if __name__ == '__main__':
+    pass

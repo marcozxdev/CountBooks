5d4ff69d79005f2ab905086d2a920c172b7fd8f9
marco salazar <marcoszxdev@gmail.com>
2026-04-26 20:06:18 -0500

    consultas en desarrollo

--- src/repository/bookRepo.py
+++ src/repository/bookRepo.py
@@ -1,31 +1,61 @@
-from database.database import Database
-# from models.bookModel import BookModel
-
+from src.database.database import Database
+from models.bookModel import BookModel
+from src.repository.sqlite import Sqlite
 class BookRepo:
     def __init__(self, db: Database):
         self.db = db
 
+    def get_books(self, quantity: int):
+        if quantity:
+            self.db.execute("SELECT * FROM libros LIMIT ?", (quantity,))
+            books = self.db.fetchall()
+            return books
+        return None
+    
 
-    def get_books(self):
-        self.db.execute("")
+    def search_book(self, book, limit=10):
+        if book:
+            book = f"%{book}%"
+            self.db.execute("""
+            SELECT * FROM libros
+            WHERE       titulo  like ? OR
+                        categoria like ? OR
+                        editorial  like ? OR
+                        codigo like ? OR
+                        referencia like ? OR
+                        estado like ? OR
+                        autor like ? 
+            LIMIT ?;
+            """,(book, book, book, book, book, book, book, limit))
+            return self.db.fetchall()
 
-    def get_book(self):
-        pass
+
+    def get_book(self, book):
+        return self.search_book(book, limit=1)
 
     
-    def add_book(self, book: dict[str, any]):
-        pass
+    def add_book(self, book: BookModel):
+        self.db.execute("""
+            INSERT INTO libros INSERT INTO libros (titulo, categoria, editorial, codigo, referencia, cantidad, estado, autor, prestado, donado, fecha) VALUES
+            (?,?,?,?,?,?,?,?,?,?,?)
+            """, (book.titulo, book.categoria, book.editorial, book.codigo, book.referencia, book.cantidad, book.estado, book.autor,  book.prestado, book.donado, book.fecha))
 
     
-    def add_books(self, books: list[dict]):
+    def add_books(self, books):
         pass
 
 
-    def update_book(self):
+    def update_book(self, book, new_book):
         pass
 
 
-    def delete_book(self):
+    def delete_book(self, book):
         pass
 
 
+
+
+#### apartado de tests
+
+query = BookRepo(Sqlite())
+print(query.search_book("cien"))

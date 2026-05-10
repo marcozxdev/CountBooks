31a08180d11b0dc66652012739f2d3f67c8aea01
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-05-09 21:02:01 -0400

    Import requests and fetch data from API
    Added requests library and made a GET request to an API.

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import pyside6 # Importamos la librería pyside6 para la UI gráfica (GUI).
+import requests
 import src/database/database.py as sql
 
 """ Este es el ejecutable de la app """
+
+"""
+
+headers = {}
+
+body = {}
+
+data = request.get("https://example.render")
+data.json()
+
+"""

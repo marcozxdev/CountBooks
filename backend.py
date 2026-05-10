e8dc5a5c0f8003b9e88bf3f1394bb14690023ddb
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-05-09 21:12:54 -0400

    Initialize FastAPI with basic endpoints

--- backend.py
+++ backend.py
@@ -1,1 +1,29 @@
-import fastapi
+from fastapi import FastAPI
+
+class DataBase:
+    def __init__(self):
+        pass
+
+app = FastAPI()
+app.title("System Books API backend")
+
+@app.get('/home', tags=["Home"])
+def home():
+    # HTML
+  
+    return {"status": "200"}
+
+@app.post('/login', tags=["Login"])
+def login(user_name: str, psswd: str):
+    # db = DataBase()
+  
+    return {"status": "201"}
+
+@app.get('/books', tags=["Books"])
+def get_books():
+    # db = DataBase()
+  
+    return {
+      "status": "200",
+      "books": "book" # En mantenimiento
+    }

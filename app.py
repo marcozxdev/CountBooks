42686c332eec9fac46a9407ce034bd734bb7b4c0
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-05-25 07:50:07 -0400

    Fix import path for database module

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pyside6 # Importamos la librería pyside6 para la UI gráfica (GUI).
 import requests
-import src/database/database.py as sql
+import src.database.database.py as sql
 
 """ Este es el ejecutable de la app """
 

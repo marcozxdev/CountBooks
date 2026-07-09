7692ebd2b22966561ac945f41f1b88a02e1b8e6e
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-07-09 18:06:03 -0400

    Fix indentation and formatting in CountBooks.py

--- CountBooks.py
+++ CountBooks.py
@@ -16,5 +16,5 @@ run_app(service=BookService(BookRepo(db)))
 
 
 """Creado con el propósito de el manejo eficiente de los inventarios de libros, permitiendo a los usuarios contar y gestionar sus colecciones de manera sencilla y organizada. Con esta aplicación, los usuarios pueden mantener un registro actualizado de sus libros, facilitando la organización y el acceso a su biblioteca personal.
-pensado en ser gratis y de codigo abierto para que cualquier persona pueda contribuir y mejorar la aplicación, fomentando una comunidad activa de desarrolladores y entusiastas de los libros. Con esta iniciativa, se busca crear una herramienta útil y accesible para todos los amantes de la lectura, promoviendo la colaboración y el intercambio de conocimientos en el ámbito de la gestión de bibliotecas personales.
+pensado en ser gratis y de código abierto para que cualquier persona pueda contribuir y mejorar la aplicación, fomentando una comunidad activa de desarrolladores y entusiastas de los libros. Con esta iniciativa, se busca crear una herramienta útil y accesible para todos los amantes de la lectura, promoviendo la colaboración y el intercambio de conocimientos en el ámbito de la gestión de bibliotecas personales.
 """
